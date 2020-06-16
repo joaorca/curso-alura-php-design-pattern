c4c86fd246ef466106b4b12c224e13b484746e1b
João R C Almeida <joaorca@gmail.com>
2020-06-16 01:03:10 -0300

    Extraindo a lógica para métodos privados

---
+++ src/Impostos/ImpostoCom2Aliquotas.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Alura\DesignPattern\Impostos;
+
+use Alura\DesignPattern\Orcamento;
+
+abstract class ImpostoCom2Aliquotas implements Imposto
+{
+    public function calculaImposto(Orcamento $orcamento) : float
+    {
+        if ($this->deveAplicatTaxaMaxima($orcamento)) {
+            return $this->calculaTaxaMaxima($orcamento);
+        }
+
+        return $this->calculaTaxaMinima($orcamento);
+    }
+
+    abstract protected function deveAplicatTaxaMaxima(Orcamento $orcamento) : bool;
+
+    abstract protected function calculaTaxaMaxima(Orcamento $orcamento) : float;
+
+    abstract protected function calculaTaxaMinima(Orcamento $orcamento) : float;
+}
