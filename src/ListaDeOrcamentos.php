ba9ef20b661490e792aabc8304097f0c65a3ba1d
João R C Almeida <joaorca@gmail.com>
2020-06-18 00:11:56 -0300

    Representando uma coleção de orçamentos

---
+++ src/ListaDeOrcamentos.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Alura\DesignPattern;
+
+class ListaDeOrcamentos
+{
+    private array $orcamentos;
+
+    public function __construct()
+    {
+        $this->orcamentos = [];
+    }
+
+    public function addOrcamento(Orcamento $orcamento)
+    {
+        $this->orcamentos[] = $orcamento;
+    }
+
+    public function orcamentos() : array
+    {
+        return $this->orcamentos;
+    }
+}
